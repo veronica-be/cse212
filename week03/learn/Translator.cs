4b4617254070231490c70e59b5dcb166f0350330
veronica-be <verobees@icloud.com>
2026-07-18 12:15:51 -0500

    MAPS

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -24,7 +24,7 @@ public class Translator
     /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words[fromWord] = toWord;
     }
 
     /// <summary>
@@ -34,7 +34,14 @@ public class Translator
     /// <returns>The translated word or "???" if no translation is available</returns>
     public string Translate(string fromWord)
     {
-        // ADD YOUR CODE HERE
-        return "";
+        try
+        {
+            string word = _words[fromWord];
+            return word;
+        }
+        catch (System.Exception)
+        {
+            return "???";
+        }
     }
 }
